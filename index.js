7c2dc24663c64c144df8025fc5a17df2a82f8de8
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-09 23:45:06 -0300

    removendo-nota.js - como utilizar o método pop() para remover o ultimo item de um array; index.js - trabalhando com array vazio

---
+++ index.js
@@ -0,0 +1,13 @@
+const arrayVazia = [];
+
+console.log(arrayVazia);
+console.log(arrayVazia.length);
+console.log(arrayVazia[0]);
+console.log(arrayVazia[1]);
+console.log(arrayVazia[2]);
+
+const arrayVazia2 = [,,,];
+console.log(arrayVazia2.length)
+arrayVazia2.push(50)
+console.log(arrayVazia2)
+console.log(arrayVazia2.length)
