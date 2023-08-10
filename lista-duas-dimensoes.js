7e9d27557ca2a2572d5b61303de5b4082c051c7a
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-10 00:36:52 -0300

    Como criar e navegar por arrays de duas ou mais dimensões.

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,8 @@
+const alunos = ["João", "Juliana", "Caio", "Ana"];
+const medias = [10, 8, 7.5, 9];
+
+const alunosEMedias = [alunos, medias];
+
+console.log(alunosEMedias);
+console.log(`Aluna da posição 1 da lista de alunos é ${alunosEMedias[0][1]}.
+A nota dessa aluna é ${alunosEMedias[1][1]}`);
