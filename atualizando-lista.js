c2086f9b2e2c6fe7a94eac74aaf735dc44efc682
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-10 00:35:56 -0300

    Utilizando o método slice() para remover e adicionar dados no array.

---
+++ atualizando-lista.js
@@ -0,0 +1,11 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+console.log(nomes);
+
+nomes.splice(1,2, 'Rodrigo');
+
+console.log(nomes);
+
+//nomes.push('Rodrigo');
+
+//console.log(nomes);
