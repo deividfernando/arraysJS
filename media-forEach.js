39cfb3a7f5da075d6f61b42b15a9dc9cb4890e83
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-14 21:32:59 -0300

    Utilizando o forEach para identar em um array. Interessante observar que, no JavaScript, diferente de outras linguagens (C#, PHP...) o foreach é um método do array.

---
+++ media-forEach.js
@@ -0,0 +1,15 @@
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+notas.forEach(function (nota, indice) {
+    //console.log('oi');
+    //console.log(nota);
+
+    somaDasNotas += nota;
+    console.log(indice);
+});
+
+const media = somaDasNotas / notas.length;
+
+console.log(`A média das notas é ${media}`);
